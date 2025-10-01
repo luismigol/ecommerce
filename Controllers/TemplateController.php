fc462e84a79a84a9e134ee8aaf21fc996616e949
= <=>
2025-10-01 10:22:22 -0500

    Iplementada plantilla adminlte

---
+++ Controllers/TemplateController.php
@@ -0,0 +1,17 @@
+<?php
+class TemplateController{
+    public function showTemplate(){
+        include "views/template.php";
+    }
+    public static function path(){
+
+        if(!empty($_SERVER["HTTPS"])&& ("on" == $_SERVER["HTTPS"])){
+                return "https://" . $_SERVER["SERVER_NAME"]. "/"; 
+
+        }else{
+                return "http://". $_SERVER["SERVER_NAME"]. "/";
+        }
+    
+    }
+
+}
